444c3909b97f92e1c8638503e2906e96c1d147b4
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-10-31 01:08:11 +0530

    refactor: cleanup comments, rearrange code

--- bin/tea.sh
+++ bin/tea.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+# home path fix for sed
+HOME_REPLACER=""
+echo "$HOME" | grep -E "^[a-zA-Z0-9\-_/.@]+$" &>/dev/null
+HOME_SED_SAFE=$?
+if [ $HOME_SED_SAFE -eq 0 ]; then
+	HOME_REPLACER="s|^$HOME/|~/|"
+fi
+
+BORDER_LABEL='   tmux-tea   '
+HEADER=" ^s sessions ^x zoxide ^f find"
+SESSION_BIND="ctrl-s:change-prompt(  )+reload(tmux list-sessions -F '#S')"
+ZOXIDE_BIND="ctrl-x:change-prompt(  )+reload(zoxide query -l | sed -e \"$HOME_REPLACER\")"
+FIND_BIND="ctrl-f:change-prompt(  )+reload(fd -H -d 2 -t d . ~)"
+TAB_BIND="tab:down,btab:up"
+PROMPT='  '
+PREVIEW="tmux list-panes -t {} -aF '#S-  #I:#W:#P   #T #{window_active}:#{pane_active}' | grep {}- | cut -d ' ' -f 2-"
+
 # determine if the tmux server is running
 if tmux list-sessions &>/dev/null; then
 	TMUX_RUNNING=0
@@ -10,21 +27,14 @@ fi
 # determine the user's current position relative tmux:
 T_RUNTYPE="serverless"
 if [ "$TMUX_RUNNING" -eq 0 ]; then
-	if [ "$TMUX" ]; then # inside tmux
+	if [ "$TMUX" ]; then
 		T_RUNTYPE="attached"
-	else # outside tmux
+	else
 		T_RUNTYPE="detached"
 	fi
 fi
 
-HOME_REPLACER=""                                          # default to a noop
-echo "$HOME" | grep -E "^[a-zA-Z0-9\-_/.@]+$" &>/dev/null # chars safe to use in sed
-HOME_SED_SAFE=$?
-if [ $HOME_SED_SAFE -eq 0 ]; then # $HOME should be safe to use in sed
-	HOME_REPLACER="s|^$HOME/|~/|"
-fi
-
-get_sessions_by_mru() {
+get_sessions_by_last_used() {
 	tmux list-sessions -F '#{session_last_attached} #{session_name}' | sort --numeric-sort --reverse | awk '{print $2}'
 }
 
@@ -37,45 +47,36 @@ get_fzf_results() {
 		fzf_default_results="$(tmux show -gqv '@t-fzf-default-results')"
 		case $fzf_default_results in
 		sessions)
-			get_sessions_by_mru
+			get_sessions_by_last_used
 			;;
 		zoxide)
 			get_zoxide_results
 			;;
 		*)
-			get_sessions_by_mru && get_zoxide_results # default shows both
+			get_sessions_by_last_used && get_zoxide_results
 			;;
 		esac
 	else
-		get_zoxide_results # only show zoxide results when outside tmux
+		get_zoxide_results
 	fi
 }
 
-BORDER_LABEL='   tmux-tea   '
-HEADER=" ^s sessions ^x zoxide ^f find"
-SESSION_BIND="ctrl-s:change-prompt(  )+reload(tmux list-sessions -F '#S')"
-ZOXIDE_BIND="ctrl-x:change-prompt(  )+reload(zoxide query -l | sed -e \"$HOME_REPLACER\")"
-FIND_BIND="ctrl-f:change-prompt(  )+reload(fd -H -d 2 -t d . ~)"
-TAB_BIND="tab:down,btab:up"
-PROMPT='  '
-PREVIEW="tmux list-panes -t {} -aF '#S-  #I:#W:#P   #T #{window_active}:#{pane_active}' | grep {}- | cut -d ' ' -f 2-"
-
-if [ $# -eq 1 ]; then # argument provided
+if [ $# -eq 1 ]; then # when t ran with argument
 	zoxide query "$1" &>/dev/null
 	ZOXIDE_RESULT_EXIT_CODE=$?
-	if [ $ZOXIDE_RESULT_EXIT_CODE -eq 0 ]; then # zoxide result found
+	if [ $ZOXIDE_RESULT_EXIT_CODE -eq 0 ]; then
 		RESULT=$(zoxide query "$1")
-	else # no zoxide result found
+	else
 		ls "$1" &>/dev/null
 		LS_EXIT_CODE=$?
-		if [ $LS_EXIT_CODE -eq 0 ]; then # directory found
+		if [ $LS_EXIT_CODE -eq 0 ]; then
 			RESULT=$1
-		else # no directory found
+		else
 			echo "No directory found."
 			exit 1
 		fi
 	fi
-else # argument not provided
+else
 	case $T_RUNTYPE in
 	attached)
 		RESULT=$(
@@ -124,38 +125,38 @@ else # argument not provided
 	esac
 fi
 
-if [ "$RESULT" = "" ]; then # no result
-	exit 0                     # exit silently
+if [ "$RESULT" = "" ]; then
+	exit 0
 fi
 
 if [ $HOME_SED_SAFE -eq 0 ]; then
-	RESULT=$(echo "$RESULT" | sed -e "s|^~/|$HOME/|") # get real home path back
+	RESULT=$(echo "$RESULT" | sed -e "s|^~/|$HOME/|")
 fi
 
-zoxide add "$RESULT" &>/dev/null # add to zoxide database
+zoxide add "$RESULT" &>/dev/null
 
-if [[ $RESULT != /* ]]; then # not folder path from zoxide result
+if [[ $RESULT != /* ]]; then # not a dir path
 	SESSION_NAME=$RESULT
 else
 	SESSION_NAME=$(basename "$RESULT" | tr ' .:' '_')
 fi
 
 if [ "$T_RUNTYPE" != "serverless" ]; then
-	SESSION=$(tmux list-sessions -F '#S' | grep "^$SESSION_NAME$") # find existing session
+	SESSION=$(tmux list-sessions -F '#S' | grep "^$SESSION_NAME$")
 fi
 
-if [ "$SESSION" = "" ]; then # session is missing
+if [ "$SESSION" = "" ]; then
 	SESSION="$SESSION_NAME"
 	if [ -e "$RESULT"/.tmuxinator.yml ]; then
 		cd "$RESULT" && tmuxinator local
 	elif [ -e "$HOME/.config/tmuxinator/$SESSION.yml" ]; then
 		tmuxinator "$SESSION"
 	else
-		tmux new-session -d -s "$SESSION" -c "$RESULT" # create session
+		tmux new-session -d -s "$SESSION" -c "$RESULT"
 	fi
 fi
 
-case $T_RUNTYPE in # attach to session
+case $T_RUNTYPE in
 attached)
 	tmux switch-client -t "$SESSION"
 	;;
@@ -164,3 +165,4 @@ serverless)
 	tmux attach -t "$SESSION"
 	;;
 esac
+
